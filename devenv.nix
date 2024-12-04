edf560da7209f2dce8f7ca7244e96a2cb16f9aca
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-12-04 02:08:20 +0200

    Enhance .gitignore and add devenv.lock file
    - Updated .gitignore to include new entries for .devenv*, devenv.local.nix, .direnv, and .pre-commit-config.yaml.
    - Added devenv.lock file to manage dependencies for the devenv environment.

---
+++ devenv.nix
@@ -0,0 +1,66 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  env.GREET = "devenv";
+
+  # packages = [ pkgs.git ];
+
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  android = {
+    enable = true;
+    flutter.enable = true;
+    platforms.version = [
+      "32"
+      "34"
+    ];
+    systemImageTypes = [ "google_apis_playstore" ];
+    abis = [
+      "arm64-v8a"
+      "x86_64"
+    ];
+    # cmakeVersions = [ "3.30.5" ];
+    cmdLineTools.version = "11.0";
+    tools.version = "26.1.1";
+    platformTools.version = "34.0.5";
+    buildTools.version = [ "30.0.3" ];
+    emulator = {
+      enable = true;
+      version = "34.1.9";
+    };
+    sources.enable = false;
+    systemImages.enable = true;
+    ndk.enable = true;
+    googleAPIs.enable = true;
+    googleTVAddOns.enable = true;
+    extras = [ "extras;google;gcm" ];
+    extraLicenses = [
+      "android-sdk-preview-license"
+      "android-googletv-license"
+      "android-sdk-arm-dbt-license"
+      "google-gdk-license"
+      "intel-android-extra-license"
+      "intel-android-sysimage-license"
+      "mips-android-sysimage-license"
+    ];
+    # android-studio = pkgs.android-studio;
+  };
+}
